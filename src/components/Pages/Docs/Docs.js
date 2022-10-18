e400657062e528b21f6d45a1800fc4e313e5c155
ilievski-david <ilievskidavid97@gmail.com>
2022-10-18 15:14:32 +0200

    typos fixed

--- src/components/Pages/Docs/Docs.js
+++ src/components/Pages/Docs/Docs.js
@@ -12,11 +12,11 @@ export default class Docs extends Component {
           </div>
           <div className='docs-box-01'>
              <div className='docs-01-number'>2</div>
-             <div className='docs-01-text'><div>Create Decentralized Oracle Dapp that will evaluate your results (<a href='https://github.com/DecentralizedAITracker/aitracker-sdk' target="_blank"> link </a>).</div> </div>
+             <div className='docs-01-text'><div>Create Decentralized Oracle Dapp that will evaluate your results.</div> </div>
           </div>
           <div className='docs-box-01'>
              <div className='docs-01-number'>3</div>
-             <div className='docs-01-text'><div>Implement aitracker-sdk in your Machine Learning Dapp and Decentalized Oracle Dapp so they can communicate privately. </div> </div>
+             <div className='docs-01-text'><div>Implement aitracker-sdk (<a href='https://github.com/DecentralizedAITracker/aitracker-sdk' target="_blank"> link </a>) in your Machine Learning Dapp and Decentalized Oracle Dapp so they can communicate privately. </div> </div>
           </div>
           <div className='docs-box-01'>
              <div className='docs-01-number'>4</div>
